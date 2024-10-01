15a95f3951b99be46ae587618fc3da9cb96eeaf0
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-10-02 00:04:49 +0300

    auth developments

---
+++ Auth/Infrastructure/Services/AuthService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Interfaces;
+using Domain.Requests;
+using Domain.Responses;
+
+namespace Infrastructure.Services;
+
+public class AuthService : IAuthService
+{
+    public Task<CustomDataResponse<LoginResponse>> Login(LoginRequest request)
+    {
+        // TODO: LoginRequest için validaasyon eklenecek
+
+        throw new NotImplementedException();
+
+    }
+}
