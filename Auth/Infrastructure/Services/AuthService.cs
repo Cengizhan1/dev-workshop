8e4623c4c00d1a0fbabba01e8615e3f7aef2db15
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-10-04 00:12:45 +0300

    keycloak config updated and refresh method added

--- Auth/Infrastructure/Services/AuthService.cs
+++ Auth/Infrastructure/Services/AuthService.cs
@@ -1,12 +1,22 @@
 ﻿using Application.Interfaces;
+using Domain.ConfigModels;
 using Domain.Requests;
 using Domain.Responses;
+using Microsoft.Extensions.Options;
 using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Infrastructure.Services;
 
 public class AuthService : IAuthService
 {
+    private readonly KeycloakOptions _config;
+    private readonly HttpClient _httpClient = new();
+    public AuthService(IOptions<KeycloakOptions> config)
+    {
+        _config = config.Value;
+    }
     public Task<CustomDataResponse<LoginResponse>> Login(LoginRequest request)
     {
         // TODO: LoginRequest için validaasyon eklenecek
@@ -18,25 +28,27 @@ public class AuthService : IAuthService
 
     public async Task<TokenResponse> Refresh(RefreshRequest request)
     {
-        var tokenEndpoint = $"{_config["Keycloak:Authority"]}/protocol/openid-connect/token";
+        var tokenEndpoint = _config.Endpoints.Token;
 
         var requestData = new FormUrlEncodedContent(new[]
         {
-            new KeyValuePair<string, string>("client_id", _config["Keycloak:ClientId"]),
-            new KeyValuePair<string, string>("client_secret", _config["Keycloak:ClientSecret"]),
-            new KeyValuePair<string, string>("grant_type", "refresh_token"),
-            new KeyValuePair<string, string>("refresh_token", refreshToken)
+            new KeyValuePair<string, string>("client_id", _config.ClientId),
+            new KeyValuePair<string, string>("client_secret", _config.ClientSecret),
+            new KeyValuePair<string, string>("grant_type", "Bearer"),
+            new KeyValuePair<string, string>("refresh_token", request.RefreshToken)
         });
 
         var response = await _httpClient.PostAsync(tokenEndpoint, requestData);
 
+
+        // TODO: Exception handle edilecek
         if (!response.IsSuccessStatusCode)
         {
             throw new Exception("Could not refresh token");
         }
 
         var content = await response.Content.ReadAsStringAsync();
-        var tokenResponse = JsonConvert.DeserializeObject<TokenResponse>(content);
+        var tokenResponse = JsonSerializer.Deserialize<TokenResponse>(content);
 
         return tokenResponse;
     }
