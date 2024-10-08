6fdbd69c9ce06077a30414f249ce1bb6699506a8
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-10-08 19:44:00 +0300

    initial commit for blog project

---
+++ Blog/Application/Interfaces/IBlogService.cs
@@ -0,0 +1,11 @@
+﻿
+using BlogDomain.Requests;
+using BlogDomain.Responses;
+using Domain.Responses;
+
+namespace Application.Interfaces;
+
+public interface IBlogService
+{
+    public Task<CustomDataResponse<BlogCreatedResponse>> Create(BlogCreateRequest request);
+}
---
+++ Blog/Domain/Entities/Blog.cs
@@ -0,0 +1,20 @@
+﻿namespace BlogDomain.Entities;
+
+public class Blog
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Content { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    // user eklenecek
+
+    // blog category eklenecek
+
+
+    
+}
