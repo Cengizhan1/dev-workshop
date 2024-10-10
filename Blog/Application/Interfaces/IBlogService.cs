8362841a6c8a7065a4730aa0f35026f791ad6813
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2024-10-10 09:58:19 +0300

    blog development

---
+++ Blog/Application/Interfaces/IBlogService.cs
@@ -0,0 +1,19 @@
+﻿
+using BlogDomain.Requests;
+using BlogDomain.Responses;
+using Domain.Responses;
+
+namespace Application.Interfaces;
+
+public interface IBlogService
+{
+    public Task<CustomDataResponse<List<BlogIndexResponse>>> GetAll(); // TODO: Filtreler eklenecek
+
+    public Task<CustomDataResponse<BlogShowResponse>> GetById(int id);
+
+    public Task<CustomDataResponse<BlogCreatedResponse>> Create(BlogCreateRequest request);
+
+    public Task<CustomDataResponse<SuccessDataResponse>> Update(BlogUpdateRequest request);
+
+    public Task<CustomDataResponse<SuccessDataResponse>> Delete(int id);
+}
---
+++ Blog/Domain/Requests/BlogCreateRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BlogDomain.Requests;
+
+public class BlogCreateRequest
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Content { get; set; }
+
+}
